a9abd8fdcc074ad7e58e2260fbfe8d8c30b42919
Julien Baladier <julien.baladier@gmail.com>
2022-11-15 18:55:26 +0100

    perf: only format on save

--- nvim/.config/nvim/lua/config/lsp/null_ls.lua
+++ nvim/.config/nvim/lua/config/lsp/null_ls.lua
@@ -5,6 +5,7 @@ end
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
   debug = true,
@@ -24,4 +25,16 @@ null_ls.setup({
     diagnostics.mypy.with({ prefer_local = ".venv/bin" }),
     formatting.stylua,
   },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 })
