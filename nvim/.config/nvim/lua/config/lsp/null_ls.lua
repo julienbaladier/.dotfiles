f411570cc6bd9345f35a34a6c6a5b9f004745dbd
Julien Baladier <julien.baladier@gmail.com>
2022-11-15 18:54:22 +0100

    perf: only run vulture and pydocstyle on save

--- nvim/.config/nvim/lua/config/lsp/null_ls.lua
+++ nvim/.config/nvim/lua/config/lsp/null_ls.lua
@@ -13,8 +13,14 @@ null_ls.setup({
     formatting.isort.with({ prefer_local = ".venv/bin" }),
     diagnostics.pyproject_flake8.with({ prefer_local = ".venv/bin" }),
     diagnostics.pylint.with({ prefer_local = ".venv/bin" }),
-    diagnostics.vulture.with({ prefer_local = ".venv/bin" }),
-    diagnostics.pydocstyle.with({ prefer_local = ".venv/bin" }),
+    diagnostics.vulture.with({
+      prefer_local = ".venv/bin",
+      method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+    }),
+    diagnostics.pydocstyle.with({
+      prefer_local = ".venv/bin",
+      method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+    }),
     diagnostics.mypy.with({ prefer_local = ".venv/bin" }),
     formatting.stylua,
   },
