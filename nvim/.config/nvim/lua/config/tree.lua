b520174111d41fafa86ffd3052c126e72cf6e48c
Julien Baladier <julien.baladier@gmail.com>
2022-11-15 19:08:47 +0100

    fix: fix nvim-tree configuration after update

--- nvim/.config/nvim/lua/config/tree.lua
+++ nvim/.config/nvim/lua/config/tree.lua
@@ -6,10 +6,6 @@ require("nvim-tree").setup({
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = false,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = true,
     icons = {
@@ -39,10 +35,8 @@ require("nvim-tree").setup({
   },
   view = {
     width = 30,
-    height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
     mappings = {
       custom_only = false,
       list = {},
